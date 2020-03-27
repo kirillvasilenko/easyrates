cc575b0ae9c0ec7f55bc0d968750ca7a35c3a814
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2020-03-27 19:40:06 +0300

    change auth to 3

--- src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
+++ src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
@@ -41,7 +41,7 @@ namespace EasyRates.ReaderApp.AspNet
                 option.LowercaseUrls = true;
             });
             ConfigureAuthentication(services);
-            ConfigureAuthorization(services);
+            ConfigureAuthorization3(services);
             
             ConfigureSwagger(services);
 
@@ -180,7 +180,7 @@ namespace EasyRates.ReaderApp.AspNet
             });
         }
 
-        private void ConfigureAuthorization(IServiceCollection services)
+        private void ConfigureAuthorization3(IServiceCollection services)
         {
             services.AddAuthorization(options =>
             {
