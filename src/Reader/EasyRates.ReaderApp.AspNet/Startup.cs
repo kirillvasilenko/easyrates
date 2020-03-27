844d079f5adfcd8bd5c0be04b7f4449aae14dffa
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2020-03-27 17:11:39 +0300

    incredible improvement

--- src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
+++ src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
@@ -189,7 +189,6 @@ namespace EasyRates.ReaderApp.AspNet
                     policy.RequireClaim(JwtClaimTypes.Role, "Admin");
                     policy.RequireClaim(JwtClaimTypes.Scope, "easyrate.reader.admin");
                 });
-
                 options.AddPolicy("Client", policy =>
                 {
                     policy.RequireClaim(JwtClaimTypes.Role, "Client", "Admin");
