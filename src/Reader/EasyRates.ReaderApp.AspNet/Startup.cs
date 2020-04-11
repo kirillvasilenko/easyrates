5703cee3f27b60b6f902506b81377dfec4847feb
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2020-04-11 10:37:14 +0300

    change code

--- src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
+++ src/Reader/EasyRates.ReaderApp.AspNet/Startup.cs
@@ -138,7 +138,6 @@ namespace EasyRates.ReaderApp.AspNet
         
         private void ConfigureAuthentication(IServiceCollection services)
         {
-            //tmp
             if (Config.GetSection("Auth").GetValue("UseTestAuth", false))
             {
                 services.AddAuthentication(options =>
